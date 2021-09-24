7ecea6ff2c5abe78c23a138abe163cffa3800d21
Student <Student@DESKTOP-KMNGNEU>
2021-09-24 15:40:38 -0500

    Commit All

---
+++ FED1StudioProject/JS/main.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    //Tables
+    $('#classTable1').DataTable();
+    $('#classTable2').DataTable();
+    $('#classTable3').DataTable();
+    $('#classTable4').DataTable();
+    $('#classTable5').DataTable();
+    $('#classTable6').DataTable();
+    //Lightbox
+    $(".thumb").on("click", function () {
+
+        var imgSrc = $(this).attr("src");
+
+        $("#lightbox-content").html('<img src="' + imgSrc + '" class="img-responsive"/>');
+
+        $("#lightbox-container").fadeIn(500);
+
+        $("#lightbox-container").on("click", function () {
+            $(this).fadeOut(500);
+        });
+    });
+});
